e1ed9287bbab4f16ee85300a698c9bb48bd9f7da
BojanaZ <bojana.zoranovic@hotmail.com>
2024-03-14 08:36:37 +0100

    Allow loading project from custom location

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,15 +1,17 @@
-from collections.abc import Iterable
 import importlib
 import pkgutil
-from pathlib import Path
 import os
+
+from pathlib import Path
+from collections.abc import Iterable
+from importlib.util import spec_from_file_location, module_from_spec
 
 
 def iterable(obj):
     return isinstance(obj, Iterable)
 
 
-def import_submodules(package, recursive=True):
+def import_submodules(package, prefix, recursive=True):
     """ Import all submodules of a module, recursively, including subpackages
 
     :param package: package (name or actual module)
@@ -17,17 +19,33 @@ def import_submodules(package, recursive=True):
     """
     if isinstance(package, str):
         package = importlib.import_module(package)
+
     results = {}
-    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + '.' + name
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__, prefix=prefix):
+
+        full_name = name
+
+        if not full_name.startswith(prefix):
+            full_name = prefix + "." + full_name
         results[full_name] = importlib.import_module(full_name)
         if recursive and is_pkg:
-            results.update(import_submodules(full_name))
+            results.update(import_submodules(full_name, full_name + ".", True))
     return results
 
 
+def import_project_from_absolute_path(project_root_path, project_module_name):
+    return import_submodules_from_absolute_path(project_root_path, project_module_name)
+
+
+def import_submodules_from_absolute_path(root_path, root_module_name):
+
+    os.chdir(root_path)
+    submodules = import_submodules(root_module_name, root_module_name+".")
+    return submodules
+
+
 def get_class_from_parent_module(class_name, package, recursive=True):
-    modules = import_submodules(package, recursive)
+    modules = import_submodules(package, "", recursive)
     for module in modules.values():
         try:
             type = getattr(module, class_name)
@@ -43,11 +61,12 @@ def get_project_root():
     return Path(__file__).parent.parent
 
 
-def get_file_path_for_format(type_name, formats, use_root=True):
+def get_file_path_for_format(type_name, formats, project_root):
 
-    root = ""
-    if use_root:
+    if project_root is None:
         root = get_project_root()
+    else:
+        root = project_root
 
     paths = {}
     for _format in formats:
